2ee0a0bdb439125b893e2152818d20a3dda18296
AlexGBT <alex.berest97@gmail.com>
2020-01-19 17:03:27 +0300

    Ready

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{--    <title>{{ config('app.name', 'Laravel') }}</title>--}}
-    <title>Instadogs</title>
+    <title>Біржа вантажів</title>
 
     <!-- Scripts -->
 
